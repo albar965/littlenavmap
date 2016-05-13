f4a45684a76b886424d325b818b608d29c6796fc
Alexander Barthel <albar965@mailbox.org>
2016-05-14 00:32:49 +0200

    Fixed elevation download problems and added marble debug switch.

--- src/main.cpp
+++ src/main.cpp
@@ -81,7 +81,7 @@ int main(int argc, char *argv[])
 
     MarbleDirs::setMarbleDataPath(QApplication::applicationDirPath() + QDir::separator() + "data");
     MarbleDirs::setMarblePluginPath(QApplication::applicationDirPath() + QDir::separator() + "plugins");
-    MarbleDebug::setEnabled(false);
+    MarbleDebug::setEnabled(s.getAndStoreValue("Options/MarbleDebug", false).toBool());
 
     qDebug() << "New Marble Local Path:" << MarbleDirs::localPath();
     qDebug() << "New Marble Plugin Local Path:" << MarbleDirs::pluginLocalPath();
