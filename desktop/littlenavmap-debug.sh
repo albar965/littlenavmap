db663ef1bbb215049c5e485034e71cb0f70ec219
Alexander Barthel <albar965@mailbox.org>
2016-05-20 12:06:23 +0200

    Added desktop entries.

---
+++ desktop/littlenavmap-debug.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=~/Programme/Marble-debug/lib:~/Qt/5.5/gcc_64/lib
+
+cd /home/alex/Projekte/littlenavmap/debug
+/home/alex/Projekte/littlenavmap/debug/littlenavmap
---
+++ desktop/littlenavmap-release.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=~/Programme/Marble-release/lib:~/Qt/5.5/gcc_64/lib
+
+cd /home/alex/Projekte/littlenavmap/release
+/home/alex/Projekte/littlenavmap/release/littlenavmap
