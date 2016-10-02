b9038ec7f71250b4159f3174da9a4b8397ddf068
Alexander Barthel <albar965@mailbox.org>
2016-10-02 13:26:59 +0200

    Created new version 1.0.3 beta. Updated changelog.

--- src/main.cpp
+++ src/main.cpp
@@ -88,7 +88,7 @@ int main(int argc, char *argv[])
   Application::setApplicationName("Little Navmap");
   Application::setOrganizationName("ABarthel");
   Application::setOrganizationDomain("abarthel.org");
-  Application::setApplicationVersion("1.0.2.beta");
+  Application::setApplicationVersion("1.0.3.beta");
 
   DatabaseManager *dbManager = nullptr;
 
