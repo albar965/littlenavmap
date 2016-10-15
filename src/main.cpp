4c2ff3606d9704f3ddf8ca401d42c30ba8d4da75
Alexander Barthel <albar965@mailbox.org>
2016-10-15 18:53:49 +0200

    New version 1.0.5

--- src/main.cpp
+++ src/main.cpp
@@ -88,7 +88,7 @@ int main(int argc, char *argv[])
   Application::setApplicationName("Little Navmap");
   Application::setOrganizationName("ABarthel");
   Application::setOrganizationDomain("abarthel.org");
-  Application::setApplicationVersion("1.0.4.beta");
+  Application::setApplicationVersion("1.0.5");
 
   DatabaseManager *dbManager = nullptr;
 
