40333c62647e2936a3ea98e5cbd13934f3e09dc8
Alexander Barthel <albar965@mailbox.org>
2016-11-28 21:15:39 +0100

    More instrumentation to catch crashes on exit.

--- src/connect/connectclient.cpp
+++ src/connect/connectclient.cpp
@@ -77,6 +77,10 @@ ConnectClient::~ConnectClient()
 
   disconnectClicked();
 
+  qDebug() << Q_FUNC_INFO << "delete dataReader";
+  delete dataReader;
+
+  qDebug() << Q_FUNC_INFO << "delete dialog";
   delete dialog;
 }
 
@@ -275,11 +279,14 @@ void ConnectClient::autoConnectToggled(bool state)
 /* Called by signal ConnectDialog::disconnectClicked */
 void ConnectClient::disconnectClicked()
 {
+  qDebug() << Q_FUNC_INFO;
+
+  reconnectNetworkTimer.stop();
+
   if(dataReader != nullptr)
     if(dataReader->isConnected())
       // Tell disconnectedFromSimulatorDirect not to reconnect
       manualDisconnect = true;
-  reconnectNetworkTimer.stop();
 
   if(dataReader != nullptr)
   {
@@ -372,6 +379,8 @@ void ConnectClient::readFromSocketError(QAbstractSocket::SocketError error)
 
 void ConnectClient::closeSocket(bool allowRestart)
 {
+  qDebug() << Q_FUNC_INFO;
+
   QAbstractSocket::SocketError error = QAbstractSocket::SocketError::UnknownSocketError;
   QString peer("Unknown"), errorStr("No error");
   if(socket != nullptr)
