5490f6cf5a6e7f28fbf18bab53f0335818560566
Alexander Barthel <albar965@mailbox.org>
2016-12-28 11:43:09 +0100

    Updated manual and version 1.2.2.beta.

--- src/main.cpp
+++ src/main.cpp
@@ -88,7 +88,7 @@ int main(int argc, char *argv[])
   Application::setApplicationName("Little Navmap");
   Application::setOrganizationName("ABarthel");
   Application::setOrganizationDomain("abarthel.org");
-  Application::setApplicationVersion("1.2.1.beta");
+  Application::setApplicationVersion("1.2.2.beta");
 
   // Start splash screen
   QPixmap pixmap(":/littlenavmap/resources/icons/splash.png");
