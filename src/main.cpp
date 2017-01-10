9fa1c273d9e9bea5649a9d41a9c4786f129399a1
Alexander Barthel <albar965@mailbox.org>
2017-01-10 22:52:05 +0100

    Merged release 1.2.4 to new 1.2.5.devel

--- src/main.cpp
+++ src/main.cpp
@@ -88,7 +88,7 @@ int main(int argc, char *argv[])
   Application::setApplicationName("Little Navmap");
   Application::setOrganizationName("ABarthel");
   Application::setOrganizationDomain("abarthel.org");
-  Application::setApplicationVersion("1.2.4");
+  Application::setApplicationVersion("1.2.5.devel");
 
   // Start splash screen
   QPixmap pixmap(":/littlenavmap/resources/icons/splash.png");
