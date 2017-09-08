80009cf18a6b78403026b1ab75313554e375baeb
Alexander Barthel <albar965@mailbox.org>
2017-09-08 18:18:54 +0200

    Version 1.6.1.beta and updated changelog.

--- src/navapp.cpp
+++ src/navapp.cpp
@@ -60,7 +60,7 @@ NavApp::NavApp(int& argc, char **argv, int flags)
   setApplicationName("Little Navmap");
   setOrganizationName("ABarthel");
   setOrganizationDomain("abarthel.org");
-  setApplicationVersion("1.6.0.beta");
+  setApplicationVersion("1.6.1.beta");
 }
 
 NavApp::~NavApp()
