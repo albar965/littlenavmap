ba07936cfe22b83315b7b26b4ef23aedc04b527d
Alexander Barthel <albar965@mailbox.org>
2017-10-04 15:30:11 +0200

    Fixed linux startup script to keep LD_LIBRARY_PATH local and added comment.

--- desktop/littlenavmap.sh
+++ desktop/littlenavmap.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=./lib
+# Adjust this script like shown below and remove "LD_LIBRARY_PATH=./lib" if you want to move it to another place
+# cd "/home/YOURUSERNAME/Little Navmap"
+# LD_LIBRARY_PATH="/home/YOURUSERNAME/Little Navmap/lib"
+
+LD_LIBRARY_PATH=./lib
 
 ./littlenavmap
