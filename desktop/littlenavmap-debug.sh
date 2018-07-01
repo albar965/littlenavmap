ecd2ba13c9bd0f9d9478870888583bb691caee51
Alexander Barthel <albar965@mailbox.org>
2018-07-01 14:04:32 +0200

    Updated Linux development start scripts to use command line parameters.

--- desktop/littlenavmap-debug.sh
+++ desktop/littlenavmap-debug.sh
@@ -3,4 +3,4 @@
 export LD_LIBRARY_PATH=~/Programme/Marble-debug/lib:~/Qt/5.9.5/gcc_64/lib
 
 cd $HOME/Projekte/build-littlenavmap-debug
-$HOME/Projekte/build-littlenavmap-debug/littlenavmap
+$HOME/Projekte/build-littlenavmap-debug/littlenavmap $@
--- desktop/littlenavmap-release.sh
+++ desktop/littlenavmap-release.sh
@@ -3,4 +3,4 @@
 export LD_LIBRARY_PATH=~/Programme/Marble-release/lib:~/Qt/5.9.5/gcc_64/lib
 
 cd $HOME/Projekte/build-littlenavmap-release
-$HOME/Projekte/build-littlenavmap-release/littlenavmap
+$HOME/Projekte/build-littlenavmap-release/littlenavmap $@
