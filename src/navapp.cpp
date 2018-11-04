39ae7c8a059c85134495dff82f9828258d02ad99
Alexander Barthel <albar965@mailbox.org>
2018-11-04 15:33:06 +0100

    Version 2.2.1.beta and updated changelog.

--- src/navapp.cpp
+++ src/navapp.cpp
@@ -86,7 +86,7 @@ NavApp::NavApp(int& argc, char **argv, int flags)
   setOrganizationName("ABarthel");
   setOrganizationDomain("abarthel.org");
 
-  setApplicationVersion("2.2.0.beta"); // VERSION_NUMBER
+  setApplicationVersion("2.2.1.beta"); // VERSION_NUMBER
 }
 
 NavApp::~NavApp()
