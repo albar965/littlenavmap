fd75baab00ec228eb5915db51bb1067e958fbcc1
Alexander Barthel <albar965@mailbox.org>
2019-04-18 17:16:23 +0200

    Fixed small issue where user point icons were too large on higher zoom levels.

--- src/mappainter/mappaintlayer.cpp
+++ src/mappainter/mappaintlayer.cpp
@@ -353,7 +353,7 @@ void MapPaintLayer::initMapLayerSettings()
          airspaceIcao(false).
          vor(false).ndb(false).waypoint(false).marker(false).ils(false).airway(false).
          airportRouteInfo(false).vorRouteInfo(false).ndbRouteInfo(false).waypointRouteName(false).
-         userpoint().userpointInfo(false).
+         userpoint().userpointInfo(false).userpoinSymbolSize(12).
          airportMaxTextLength(16)).
 
   // Display only points for airports until the cutoff limit
@@ -371,7 +371,7 @@ void MapPaintLayer::initMapLayerSettings()
          airspaceRestricted(false).airspaceSpecial(false).airspaceIcao(false).
          vor(false).ndb(false).waypoint(false).marker(false).ils(false).airway(false).
          airportRouteInfo(false).vorRouteInfo(false).ndbRouteInfo(false).waypointRouteName(false).
-         userpoint().userpointInfo(false).
+         userpoint().userpointInfo(false).userpoinSymbolSize(12).
          airportMaxTextLength(16)).
 
   // Make sure that there is always an layer
@@ -388,7 +388,7 @@ void MapPaintLayer::initMapLayerSettings()
          airspaceRestricted(false).airspaceSpecial(false).airspaceIcao(false).
          airport(false).vor(false).ndb(false).waypoint(false).marker(false).ils(false).airway(false).
          airportRouteInfo(false).vorRouteInfo(false).ndbRouteInfo(false).waypointRouteName(false).
-         userpoint(false).userpointInfo(false).
+         userpoint(false).userpointInfo(false).userpoinSymbolSize(12).
          airportMaxTextLength(16));
 
   // Sort layers
