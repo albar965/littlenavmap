396ddd2bd775540e7a1de237d627737da865576c
Alexander Barthel <albar965@mailbox.org>
2019-05-04 12:54:59 +0200

    Fixed occasional crashes when drawing.

--- src/mappainter/mappainteruser.cpp
+++ src/mappainter/mappainteruser.cpp
@@ -39,7 +39,7 @@ using namespace Marble;
 using namespace atools::geo;
 using namespace map;
 
-MapPainterUser::MapPainterUser(MapPaintWidget* mapWidget, MapScale *mapScale)
+MapPainterUser::MapPainterUser(MapPaintWidget *mapWidget, MapScale *mapScale)
   : MapPainter(mapWidget, mapScale)
 {
 }
@@ -89,8 +89,9 @@ void MapPainterUser::paintUserpoints(PaintContext *context, const QList<MapUserp
           y += size / 2.f;
         }
 
-        context->painter->drawPixmap(QPointF(x - size / 2.f, y - size / 2.f),
-                                     *icons->getIconPixmap(userpoint.type, atools::roundToInt(size)));
+        if(x < INVALID_INDEX_VALUE / 2 && y < INVALID_INDEX_VALUE / 2)
+          context->painter->drawPixmap(QPointF(x - size / 2.f, y - size / 2.f),
+                                       *icons->getIconPixmap(userpoint.type, atools::roundToInt(size)));
 
         if(context->mapLayer->isUserpointInfo() && !drawFast)
         {
