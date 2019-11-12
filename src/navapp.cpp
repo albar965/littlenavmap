8b6da5ea94087f746d5ce4dbc88ea6d8e06f01e3
Alexander Barthel <albar965@mailbox.org>
2019-11-12 16:48:45 +0100

    Version 2.5.0.develop

--- src/navapp.cpp
+++ src/navapp.cpp
@@ -101,7 +101,7 @@ void NavApp::initApplication()
   setApplicationName("Little Navmap");
   setOrganizationName("ABarthel");
   setOrganizationDomain("littlenavmap.org");
-  setApplicationVersion("2.4.4"); // VERSION_NUMBER - Little Navmap
+  setApplicationVersion("2.5.0.develop"); // VERSION_NUMBER - Little Navmap
 }
 
 NavApp *NavApp::navAppInstance()
