f47f484ce5803b4ac52ab81bf87ff4965759fc01
Alexander Barthel <albar965@mailbox.org>
2020-05-28 16:29:47 +0200

    Updated execution scripts for Qt 5.12.8.

--- desktop/littlenavmap-release.sh
+++ desktop/littlenavmap-release.sh
@@ -6,6 +6,6 @@ CONF=release
 
 cd ${APROJECTS}/build-littlenavmap-${CONF}
 
-export LD_LIBRARY_PATH=${APROJECTS}/Marble-${CONF}/lib:~/Qt/5.12.5/gcc_64/lib:${APROJECTS}/build-littlenavmap-${CONF}
+export LD_LIBRARY_PATH=${APROJECTS}/Marble-${CONF}/lib:~/Qt/5.12.8/gcc_64/lib:${APROJECTS}/build-littlenavmap-${CONF}
 
 ${APROJECTS}/build-littlenavmap-${CONF}/littlenavmap $@
