0bc39005b36c16dcb4b012abcc15f7451876d310
Alexander Barthel <albar965@mailbox.org>
2020-11-05 23:26:11 +0100

    2.6.4.beta and updated changelog.

--- src/navapp.cpp
+++ src/navapp.cpp
@@ -107,7 +107,7 @@ void NavApp::initApplication()
   setApplicationName("Little Navmap");
   setOrganizationName("ABarthel");
   setOrganizationDomain("littlenavmap.org");
-  setApplicationVersion("2.6.3.beta"); // VERSION_NUMBER - Little Navmap
+  setApplicationVersion("2.6.4.beta"); // VERSION_NUMBER - Little Navmap
 }
 
 NavApp *NavApp::navAppInstance()
