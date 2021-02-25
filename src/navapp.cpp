b3a4bcc1393d0614936b22700f87b4535b9d98c9
Alexander Barthel <albar965@mailbox.org>
2021-02-25 14:57:06 +0100

    2.6.10

--- src/navapp.cpp
+++ src/navapp.cpp
@@ -108,7 +108,7 @@ void NavApp::initApplication()
   setApplicationName("Little Navmap");
   setOrganizationName("ABarthel");
   setOrganizationDomain("littlenavmap.org");
-  setApplicationVersion("2.6.9"); // VERSION_NUMBER - Little Navmap
+  setApplicationVersion("2.6.10"); // VERSION_NUMBER - Little Navmap
 }
 
 NavApp *NavApp::navAppInstance()
