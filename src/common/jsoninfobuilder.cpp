060a45f30bfd208fe65a0c6477dc70169994bbe2
Kosma Kaczmarski <info@koka-produktion.de>
2021-07-08 16:08:22 +0200

    Shorten code
    - alias airportInfoData as data

--- src/common/jsoninfobuilder.cpp
+++ src/common/jsoninfobuilder.cpp
@@ -34,15 +34,18 @@ JsonInfoBuilder::~JsonInfoBuilder()
 
 QByteArray JsonInfoBuilder::airport(AirportInfoData airportInfoData) const
 {
+
+    AirportInfoData& data = airportInfoData;
+
     JSON json = {
-        { "ident", qUtf8Printable(airportInfoData.airport.ident) },
-        { "ICAO", qUtf8Printable(airportInfoData.airport.icao) },
-        { "name", qUtf8Printable(airportInfoData.airport.name) },
-        { "region", qUtf8Printable(airportInfoData.airport.region) },
-        { "closed", airportInfoData.airport.closed() },
-        { "addon", airportInfoData.airport.addon() },
-        { "elevation", qUtf8Printable(Unit::altFeet(airportInfoData.airport.getPosition().getAltitude())) },
-        { "magneticDeclination", qUtf8Printable(map::magvarText(airportInfoData.airport.magvar)) },
+        { "ident", qUtf8Printable(data.airport.ident) },
+        { "ICAO", qUtf8Printable(data.airport.icao) },
+        { "name", qUtf8Printable(data.airport.name) },
+        { "region", qUtf8Printable(data.airport.region) },
+        { "closed", data.airport.closed() },
+        { "addon", data.airport.addon() },
+        { "elevation", qUtf8Printable(Unit::altFeet(data.airport.getPosition().getAltitude())) },
+        { "magneticDeclination", qUtf8Printable(map::magvarText(data.airport.magvar)) },
         { "rating", nullptr },
         { "IATA", nullptr },
         { "city", nullptr },
@@ -59,138 +62,138 @@ QByteArray JsonInfoBuilder::airport(AirportInfoData airportInfoData) const
 
     /* Null fields populated if available */
 
-    if(airportInfoData.airportInformation!=nullptr){
-        json["rating"] = airportInfoData.airportInformation->valueInt("rating");
-        json["IATA"] = qUtf8Printable(airportInfoData.airportInformation->valueStr("iata"));
+    if(data.airportInformation!=nullptr){
+        json["rating"] = data.airportInformation->valueInt("rating");
+        json["IATA"] = qUtf8Printable(data.airportInformation->valueStr("iata"));
     }
 
-    if(airportInfoData.airportAdminNames!=nullptr){
-        json["city"] = qUtf8Printable(airportInfoData.airportAdminNames->city);
-        json["state"] = qUtf8Printable(airportInfoData.airportAdminNames->state);
-        json["country"] = qUtf8Printable(airportInfoData.airportAdminNames->country);
+    if(data.airportAdminNames!=nullptr){
+        json["city"] = qUtf8Printable(data.airportAdminNames->city);
+        json["state"] = qUtf8Printable(data.airportAdminNames->state);
+        json["country"] = qUtf8Printable(data.airportAdminNames->country);
     }
 
-    if(airportInfoData.transitionAltitude!=nullptr && *airportInfoData.transitionAltitude > 0){
-        json["transitionAltitude"] = qUtf8Printable(Unit::altFeet(*airportInfoData.transitionAltitude));
+    if(data.transitionAltitude!=nullptr && *data.transitionAltitude > 0){
+        json["transitionAltitude"] = qUtf8Printable(Unit::altFeet(*data.transitionAltitude));
     }
 
-    if(!airportInfoData.airport.noRunways()){
-        json["longestRunwayLength"] = qUtf8Printable(Unit::distShortFeet(airportInfoData.airport.longestRunwayLength));
-        if(airportInfoData.airportInformation!=nullptr){
-            json["longestRunwayWidth"] = qUtf8Printable(Unit::distShortFeet(airportInfoData.airportInformation->valueInt("longest_runway_width")));
-            json["longestRunwayHeading"] = qUtf8Printable(getHeadingsStringByMagVar(airportInfoData.airportInformation->valueFloat("longest_runway_heading"),airportInfoData.airport.magvar));
-            json["longestRunwaySurface"] = qUtf8Printable(airportInfoData.airportInformation->valueStr("longest_runway_surface"));
+    if(!data.airport.noRunways()){
+        json["longestRunwayLength"] = qUtf8Printable(Unit::distShortFeet(data.airport.longestRunwayLength));
+        if(data.airportInformation!=nullptr){
+            json["longestRunwayWidth"] = qUtf8Printable(Unit::distShortFeet(data.airportInformation->valueInt("longest_runway_width")));
+            json["longestRunwayHeading"] = qUtf8Printable(getHeadingsStringByMagVar(data.airportInformation->valueFloat("longest_runway_heading"),data.airport.magvar));
+            json["longestRunwaySurface"] = qUtf8Printable(data.airportInformation->valueStr("longest_runway_surface"));
         }
     }
 
     /* METAR */
 
     // Simulator
-    if(!airportInfoData.weatherContext.fsMetar.isEmpty()){
+    if(!data.weatherContext.fsMetar.isEmpty()){
         json["metar"].push_back({{ "simulator", {
-                {"station", qUtf8Printable(airportInfoData.weatherContext.fsMetar.metarForStation)},
-                {"nearest", qUtf8Printable(airportInfoData.weatherContext.fsMetar.metarForNearest)},
-                {"interpolated", qUtf8Printable(airportInfoData.weatherContext.fsMetar.metarForInterpolated)},
+                {"station", qUtf8Printable(data.weatherContext.fsMetar.metarForStation)},
+                {"nearest", qUtf8Printable(data.weatherContext.fsMetar.metarForNearest)},
+                {"interpolated", qUtf8Printable(data.weatherContext.fsMetar.metarForInterpolated)},
             }
         }});
     }
 
     // Active Sky
-    if(!airportInfoData.weatherContext.asMetar.isEmpty()){
+    if(!data.weatherContext.asMetar.isEmpty()){
         json["metar"].push_back({{ "activesky", {
-                {"station", qUtf8Printable(airportInfoData.weatherContext.asMetar)},
+                {"station", qUtf8Printable(data.weatherContext.asMetar)},
             }
         }});
     }
 
     // NOAA
     if(
-       !airportInfoData.weatherContext.noaaMetar.metarForStation.isEmpty() ||
-       !airportInfoData.weatherContext.noaaMetar.metarForNearest.isEmpty()
+       !data.weatherContext.noaaMetar.metarForStation.isEmpty() ||
+       !data.weatherContext.noaaMetar.metarForNearest.isEmpty()
       ){
         json["metar"].push_back({{ "noaa", {
-                {"station", qUtf8Printable(airportInfoData.weatherContext.noaaMetar.metarForStation)},
-                {"nearest", qUtf8Printable(airportInfoData.weatherContext.noaaMetar.metarForNearest)},
+                {"station", qUtf8Printable(data.weatherContext.noaaMetar.metarForStation)},
+                {"nearest", qUtf8Printable(data.weatherContext.noaaMetar.metarForNearest)},
             }
         }});
     }
 
     // VATSIM
     if(
-       !airportInfoData.weatherContext.vatsimMetar.metarForStation.isEmpty() ||
-       !airportInfoData.weatherContext.vatsimMetar.metarForNearest.isEmpty()
+       !data.weatherContext.vatsimMetar.metarForStation.isEmpty() ||
+       !data.weatherContext.vatsimMetar.metarForNearest.isEmpty()
       ){
         json["metar"].push_back({{ "vatsim", {
-                {"station", qUtf8Printable(airportInfoData.weatherContext.vatsimMetar.metarForStation)},
-                {"nearest", qUtf8Printable(airportInfoData.weatherContext.vatsimMetar.metarForNearest)},
+                {"station", qUtf8Printable(data.weatherContext.vatsimMetar.metarForStation)},
+                {"nearest", qUtf8Printable(data.weatherContext.vatsimMetar.metarForNearest)},
             }
         }});
     }
 
     // IVAO
     if(
-       !airportInfoData.weatherContext.ivaoMetar.metarForStation.isEmpty() ||
-       !airportInfoData.weatherContext.ivaoMetar.metarForStation.isEmpty()
+       !data.weatherContext.ivaoMetar.metarForStation.isEmpty() ||
+       !data.weatherContext.ivaoMetar.metarForStation.isEmpty()
       ){
         json["metar"].push_back({{"ivao", {
-                {"station", qUtf8Printable(airportInfoData.weatherContext.ivaoMetar.metarForStation)},
-                {"nearest", qUtf8Printable(airportInfoData.weatherContext.ivaoMetar.metarForNearest)},
+                {"station", qUtf8Printable(data.weatherContext.ivaoMetar.metarForStation)},
+                {"nearest", qUtf8Printable(data.weatherContext.ivaoMetar.metarForNearest)},
             }
         }});
     }
 
     /* Facilities */
 
-    if(airportInfoData.airport.closed())
+    if(data.airport.closed())
         json["facilities"].push_back(qUtf8Printable(tr("Closed")));
-    if(airportInfoData.airport.addon())
+    if(data.airport.addon())
         json["facilities"].push_back(qUtf8Printable(tr("Add-on")));
-    if(airportInfoData.airport.is3d())
+    if(data.airport.is3d())
         json["facilities"].push_back(qUtf8Printable(tr("3D")));
-    if(airportInfoData.airport.flags.testFlag(map::MapAirportFlag::AP_MIL))
+    if(data.airport.flags.testFlag(map::MapAirportFlag::AP_MIL))
         json["facilities"].push_back(qUtf8Printable(tr("Military")));
-    if(airportInfoData.airport.apron())
+    if(data.airport.apron())
         json["facilities"].push_back(qUtf8Printable(tr("Aprons")));
-    if(airportInfoData.airport.taxiway())
+    if(data.airport.taxiway())
         json["facilities"].push_back(qUtf8Printable(tr("Taxiways")));
-    if(airportInfoData.airport.towerObject())
+    if(data.airport.towerObject())
         json["facilities"].push_back(qUtf8Printable(tr("Tower Object")));
-    if(airportInfoData.airport.parking())
+    if(data.airport.parking())
         json["facilities"].push_back(qUtf8Printable(tr("Parking")));
-    if(airportInfoData.airport.helipad())
+    if(data.airport.helipad())
         json["facilities"].push_back(qUtf8Printable(tr("Helipads")));
-    if(airportInfoData.airport.flags.testFlag(map::MapAirportFlag::AP_AVGAS))
+    if(data.airport.flags.testFlag(map::MapAirportFlag::AP_AVGAS))
         json["facilities"].push_back(qUtf8Printable(tr("Avgas")));
-    if(airportInfoData.airport.flags.testFlag(map::MapAirportFlag::AP_JETFUEL))
+    if(data.airport.flags.testFlag(map::MapAirportFlag::AP_JETFUEL))
         json["facilities"].push_back(qUtf8Printable(tr("Jetfuel")));
-    if(airportInfoData.airport.flags.testFlag(map::MapAirportFlag::AP_ILS))
+    if(data.airport.flags.testFlag(map::MapAirportFlag::AP_ILS))
         json["facilities"].push_back(qUtf8Printable(tr("ILS")));
-    if(airportInfoData.airport.vasi())
+    if(data.airport.vasi())
         json["facilities"].push_back(qUtf8Printable(tr("VASI")));
-    if(airportInfoData.airport.als())
+    if(data.airport.als())
         json["facilities"].push_back(qUtf8Printable(tr("ALS")));
-    if(airportInfoData.airport.flatten == 1)
+    if(data.airport.flatten == 1)
         json["facilities"].push_back(qUtf8Printable(tr("Flatten")));
-    if(airportInfoData.airport.flatten == 0)
+    if(data.airport.flatten == 0)
         json["facilities"].push_back(qUtf8Printable(tr("No Flatten")));
 
     /* Runways */
 
-    if(airportInfoData.airport.hard())
+    if(data.airport.hard())
         json["runways"].push_back(qUtf8Printable(tr("Hard")));
-    if(airportInfoData.airport.soft())
+    if(data.airport.soft())
         json["runways"].push_back(qUtf8Printable(tr("Soft")));
-    if(airportInfoData.airport.water())
+    if(data.airport.water())
         json["runways"].push_back(qUtf8Printable(tr("Water")));
-    if(airportInfoData.airport.closedRunways())
+    if(data.airport.closedRunways())
         json["runways"].push_back(qUtf8Printable(tr("Closed")));
-    if(airportInfoData.airport.flags.testFlag(map::MapAirportFlag::AP_LIGHT))
+    if(data.airport.flags.testFlag(map::MapAirportFlag::AP_LIGHT))
         json["runways"].push_back(qUtf8Printable(tr("Lighted")));
 
     /* Fields added only if available */
 
-    if(airportInfoData.airport.xpident.length() > 0 && airportInfoData.airport.ident != airportInfoData.airport.xpident)
-        json["x-plane-ident"] = qUtf8Printable(airportInfoData.airport.xpident);
+    if(data.airport.xpident.length() > 0 && data.airport.ident != data.airport.xpident)
+        json["x-plane-ident"] = qUtf8Printable(data.airport.xpident);
 
     return json.dump().data();
 }
