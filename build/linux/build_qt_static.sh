293817ae4394c2593fd36b3c6b6eaf4dd7c85c96
Alexander Barthel <albar965@mailbox.org>
2022-11-17 14:24:52 +0100

    Removed SSL from static build

--- build/linux/build_qt_static.sh
+++ build/linux/build_qt_static.sh
@@ -21,7 +21,7 @@ set -e
 
 cd  $APROJECTS/qt-everywhere-src-5.15.2
 
-./configure -openssl-linked -static -prefix ../build-qt-5.15.2-release -opensource -nomake examples -nomake tests -confirm-license -skip qtwayland -no-gui -no-widgets -no-dbus -qt-zlib -no-libpng -no-libjpeg -no-sqlite -no-freetype -no-harfbuzz -no-icu -no-iconv -nomake tools -no-evdev -no-accessibility -no-opengl -skip multimedia -no-qpa -skip tools -no-pch -no-kms -no-directfb -no-eglfs -no-xcb -no-cups -no-gif -no-tiff -no-ico -no-webp -skip declarative -skip location -skip serialbus
+./configure -static -prefix ../build-qt-5.15.2-release -opensource -nomake examples -nomake tests -confirm-license -skip qtwayland -no-gui -no-widgets -no-dbus -qt-zlib -no-openssl -no-libpng -no-libjpeg -no-sqlite -no-freetype -no-harfbuzz -no-icu -no-iconv -nomake tools -no-evdev -no-accessibility -no-opengl -skip multimedia -no-qpa -skip tools -no-pch -no-kms -no-directfb -no-eglfs -no-xcb -no-cups -no-gif -no-tiff -no-ico -no-webp -skip declarative -skip location -skip serialbus
 
 cd  $APROJECTS/qt-everywhere-src-5.15.2
 
