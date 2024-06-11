fbaac84b47b870b8382f7e9b8449ae02c8913e9e
u-an-i <84718885+u-an-i@users.noreply.github.com>
2024-06-11 02:35:46 +0200

    start update plugin interface
    step 2 a ii

--- web/javascript_and_css/2021-a/plugins.js
+++ web/javascript_and_css/2021-a/plugins.js
@@ -2,6 +2,8 @@ function findPlugins() {
   new (function() {
     var we = this;
     
+    var pluginsConfig = {};
+    
     var currentlyStartedPlugin;
     var stopMethods = {};
 
@@ -246,93 +248,191 @@ function findPlugins() {
       if(plugin?.tagName?.toLowerCase() === "input") {
         if(plugin.checked) {
           var iframe = document.createElement("iframe");
+          iframe.style = "position:absolute;visibility:hidden;pointer-events:none;top:0;left:0;z-index:-1;width:1px;height:1px";
+          switch(pluginsConfig[plugin.value].type) {
+          	case "background":
+          		iframe.setAttribute("data-type", "background");
+          		break;
+          	case "unobtrusive":
+          		iframe.className = "overlaying-content";
+              iframe.setAttribute("data-type", "unobtrusive");
+              iframe.setAttribute("allowtransparency", "true");   // appears to be unnecessary
+              iframe.style.width = "100%";
+              iframe.style.height = "calc(100% - 50px)";    // 50px is height of map HTML #header
+              iframe.style.top = "50px";
+              iframe.style.visibility = "visible";
+              iframe.style.zIndex = "";
+              break;
+            case "exclusive":
+           		iframe.className = "displayable-content";
+              iframe.setAttribute("data-type", "exclusive");
+              iframe.setAttribute("allowtransparency", "false");   // appears to have no effect
+              iframe.style.background = "#fff";
+              iframe.style.pointerEvents = "all";
+              iframe.style.width = "100%";
+              iframe.style.height = "100%";
+              iframe.style.visibility = "visible";
+              iframe.style.zIndex = "";
+              if(document.querySelector("#exclusivePlugins") === null) {
+                var select = document.createElement("select");
+                select.id = "exclusivePlugins";
+                var option = document.createElement("option");
+                option.innerText = "no exclusive plugin";
+                option.value = "/";
+                select.add(option);
+                option = document.createElement("option");
+                option.innerText = currentlyStartedPlugin;
+                option.value = currentlyStartedPlugin;
+                select.add(option);
+                document.querySelector("#toolbarOuter").appendChild(select);
+                select.onchange = function() {
+                  Array.prototype.forEach.call(document.querySelectorAll(".displayable-content"), function(dc) {
+                    dc.style.display = "none";
+                  });
+                  if(this.value !== "/") {
+                    document.querySelector('iframe[data-id="' + this.value + '"]').style.display = "block";
+                  }
+                  storeState("activeExclusivePlugin", this.value);
+                };
+              } else {
+                var option = document.createElement("option");
+                option.innerText = currentlyStartedPlugin;
+                option.value = currentlyStartedPlugin;
+                var select = document.querySelector("#exclusivePlugins");
+                select.add(option);
+              }
+              select.selectedIndex = select.childElementCount - 1;
+              select.dispatchEvent(new Event("change"));
+              break;
+            case "widget":
+            	iframe.className = "movable-content resizable-content";
+              iframe.setAttribute("data-type", "widget");
+              iframe.setAttribute("allowtransparency", "false");   // appears to be unnecessary
+              iframe.style.background = "#fff";
+              iframe.style.pointerEvents = "all";
+              var contentContainer = document.getElementById("contentContainer");
+              var settings = retrieveState("plugin_" + currentlyStartedPlugin);
+              iframe.style.width = settings?.width ?? "205px";
+              iframe.style.height = settings?.height ?? "225px";
+              iframe.style.top = settings?.top ?? "100px";    // 50px is height of map HTML #header
+              iframe.style.left = settings?.left ?? "50px";
+              iframe.style.bottom = settings?.bottom ?? "auto";
+              iframe.style.right = settings?.right ?? "auto";
+              // 20 = iframe border-top
+              if(iframe.offsetTop + 20 >= contentContainer.clientHeight - iframe.clientHeight) {
+                iframe.style.top = "auto";
+                iframe.style.bottom = "0";
+                storeState("plugin_" + currentlyStartedPlugin, settings);
+              }
+              if(iframe.offsetLeft >= contentContainer.clientWidth - iframe.clientWidth) {
+                iframe.style.left = "auto";
+                iframe.style.right = "0";
+                storeState("plugin_" + currentlyStartedPlugin, settings);
+              }
+              // 50 = map toolbar height (without toolbar scrollbar)
+              if(iframe.clientHeight > contentContainer.clientHeight - 50) {
+                iframe.style.height = (contentContainer.clientHeight - 50) / 2 + "px";
+                storeState("plugin_" + currentlyStartedPlugin, settings);
+              }
+              if(iframe.clientWidth > contentContainer.clientWidth) {
+                iframe.style.width = contentContainer.clientWidth / 2 + "px";
+                storeState("plugin_" + currentlyStartedPlugin, settings);
+              }
+              iframe.style.visibility = "visible";
+              iframe.style.zIndex = "";
+              break;
+            default:
+            	return;
+          }
           iframe.setAttribute("data-id", plugin.value);
           iframe.setAttribute("allow", "accelerometer *; ambient-light-sensor *; attribution-reporting *; autoplay *; battery *; bluetooth *; browsing-topics *; camera *; compute-pressure *; display-capture *; document-domain *; encrypted-media *; execution-while-not-rendered *; execution-while-out-of-viewport *; fullscreen *; gamepad *; geolocation *; gyroscope *; hid *; identity-credentials-get *; idle-detection *; local-fonts *; magnetometer *; microphone *; midi *; otp-credentials *; payment *; picture-in-picture *; publickey-credentials-create *; publickey-credentials-get *; screen-wake-lock *; serial *; speaker-selection *; storage-access *; usb *; web-share *; window-management *; xr-spatial-tracking *");
-          iframe.setAttribute("sandbox", "allow-forms allow-modals allow-orientation-lock allow-presentation allow-scripts allow-same-origin");   // should be without allow-same-origin but dynamic switching not supported
-          iframe.onload = initPlugin;
-          iframe.style = "position:absolute;visibility:hidden;pointer-events:none;top:0;left:0;z-index:-1;width:1px;height:1px";
-          iframe.src = "/plugins/" + plugin.value + "/index.html";
+          if(pluginsConfig[plugin.value].requestMapAccess.length) {
+	          if(retrieveState("mapAccessGranted_plugin_" + plugin.value, 0, true) === "true" || confirm("The plugin\n\n\"" + pluginsConfig[plugin.value].title + "\"\n\nis requesting access to the map.\n\nReason given:\n\n\"" + pluginsConfig[plugin.value].requestMapAccess + "\"\n\nGrant it?\n\n\nNote:\n\nThe plugin then will be able to access anything else inside the Little Navmap web frontend.")) {
+	            storeState("mapAccessGranted_plugin_" + plugin.value, "true", true);
+		          iframe.setAttribute("sandbox", "allow-forms allow-modals allow-orientation-lock allow-presentation allow-scripts allow-same-origin");
+		          iframe.onload = function() {
+		          	this.contentWindow.setMapWindow(document.querySelector("iframe[name=contentiframe]").contentDocument);
+		          };
+	          } else {
+		          iframe.setAttribute("sandbox", "allow-forms allow-modals allow-orientation-lock allow-presentation allow-scripts");
+	          }
+	        }
           document.querySelector("#contentContainer").insertBefore(iframe, document.querySelector('iframe[name="addoniframe"]'));
+          iframe.src = "/plugins/" + plugin.value + "/index.html";
+          activePlugins.push(currentlyStartedPlugin);
+	        storeState("activePlugins", activePlugins);
+	        loadingOnInit ? continueLoadingPlugins() : !1;
         } else {
-          try {
-            stopMethods[plugin.value]();
-          } catch(e) {}
           Array.prototype.forEach.call(document.querySelector('iframe[name="contentiframe"]').contentDocument.querySelectorAll('#pluginform [data-source="' + plugin.value + '"]'), function(toolbarEntry) {
             toolbarEntry.parentElement.removeChild(toolbarEntry);
           });
           var iframe = document.querySelector('iframe[data-id="' + plugin.value + '"]');
-          iframe.parentElement.removeChild(iframe);
-          var select = document.querySelector("#exclusivePlugins");
-          if(select !== null) {
-            var option = select.querySelector('option[value="' + plugin.value + '"]');
-            if(option !== null) {
-              select.remove(Array.prototype.indexOf.call(select, option));
-              if(select.childElementCount < 2) {
-                select.parentElement.removeChild(select);
-              }
-            }
-          }
-          var spliceIndex = activePlugins.indexOf(plugin.value);
-          if(spliceIndex > -1) {
-            activePlugins.splice(spliceIndex, 1);
-            storeState("activePlugins", activePlugins);
-          }
+          iframe.postMessage({pluginParent: {message: {cause: "cleanup"}}});
+          setTimeout(() => {
+	          iframe.remove();
+	          var select = document.querySelector("#exclusivePlugins");
+	          if(select !== null) {
+	            var option = select.querySelector('option[value="' + plugin.value + '"]');
+	            if(option !== null) {
+	              select.remove(Array.prototype.indexOf.call(select, option));
+	              if(select.childElementCount < 2) {
+	                select.parentElement.removeChild(select);
+	              }
+	            }
+	          }
+	          var spliceIndex = activePlugins.indexOf(plugin.value);
+	          if(spliceIndex > -1) {
+	            activePlugins.splice(spliceIndex, 1);
+	            storeState("activePlugins", activePlugins);
+	          }
+          }, 500;
         }
       }
     }
 
     var pluginbutton = document.querySelector("#buttonPlugins");
     if(pluginbutton === null) {
-      fetch("/plugins").then(function(response) {
-        return response.text();
-      }).then(function(text) {
+      fetch("/plugins").then(response => response.text()).then(text => {
         var pluginlist = document.createElement("div");
+        var configs = [];
         text.split("/").forEach(function(dir) {
           if(dir.substring(0, "example-".length) !== "example-" || location.search.substring(1) === "debug") {
-            var plugin = document.createElement("div");
-            plugin.className = "selectable-plugin";
-            var checkbox = document.createElement("input");
-            var id = "a" + (Math.random() + "").substring(2);
-            checkbox.type = "checkbox";
-            checkbox.id = id;
-            checkbox.value = dir;
-            var label = document.createElement("label");
-            label.setAttribute("for", id);
-            label.innerText = dir;
-            plugin.appendChild(checkbox);
-            plugin.appendChild(label);
-            pluginlist.appendChild(plugin);
+          	configs.push(fetch("/plugins/" + dir + "/config.json").then(response => response.json()).then(json => {
+	            pluginsConfig[dir] = json;
+	            var plugin = document.createElement("div");
+	            plugin.className = "selectable-plugin plugin-" + json.type;
+	            var checkbox = document.createElement("input");
+	            var id = "a" + (Math.random() + "").substring(2);
+	            checkbox.type = "checkbox";
+	            checkbox.id = id;
+	            checkbox.value = dir;
+	            var label = document.createElement("label");
+	            label.setAttribute("for", id);
+	            label.innerText = json.title;
+	            plugin.appendChild(checkbox);
+	            plugin.appendChild(label);
+	            pluginlist.appendChild(plugin);
+			      }));
           }
         });
-        if(pluginlist.childElementCount) {
-          Object.keys(types).forEach(type => {
-            var typeFunc = (function(typeFunc, context) {
-              return function() {
-                if(this === context) {
-                  return typeFunc();
-                }
-                console?.error("no");
-                return;
-              };
-            })(types[type], we);
-            typeFunc.id = type.toUpperCase();
-            allowedTypes.push(typeFunc);
-          });
-          pluginlist.id = "selectablePlugins";
-          var menuentry = document.createElement("button");
-          menuentry.id = "buttonPlugins";
-          menuentry.setAttribute("data-activateable", "false");
-          var menuname = document.createElement("span");
-          menuname.innerText = "Plugins";
-          menuentry.appendChild(menuname);
-          menuentry.appendChild(pluginlist);
-          document.querySelector("#toolbarOuter").appendChild(menuentry);
-          pluginlist.addEventListener("click", loadPlugin);
-          wasActivePlugins = retrieveState("activePlugins", []);
-          activeExclusivePlugin = retrieveState("activeExclusivePlugin", "/")
-          loadingOnInit = true;
-          continueLoadingPlugins(true);
-        }
+        Promise.all(configs).then(() => {
+	        if(pluginlist.childElementCount) {
+	          pluginlist.id = "selectablePlugins";
+	          var menuentry = document.createElement("button");
+	          menuentry.id = "buttonPlugins";
+	          menuentry.setAttribute("data-activateable", "false");
+	          var menuname = document.createElement("span");
+	          menuname.innerText = "Plugins";
+	          menuentry.appendChild(menuname);
+	          menuentry.appendChild(pluginlist);
+	          document.querySelector("#toolbarOuter").appendChild(menuentry);
+	          pluginlist.addEventListener("click", loadPlugin);
+	          wasActivePlugins = retrieveState("activePlugins", []);
+	          activeExclusivePlugin = retrieveState("activeExclusivePlugin", "/")
+	          loadingOnInit = true;
+	          continueLoadingPlugins(true);
+	        }
+        });
       });
     }
   })();
