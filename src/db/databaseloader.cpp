575ea55635c733100b9836af5f3eb72700d8a4e8
Alexander Barthel <albar965@mailbox.org>
2025-01-06 22:46:29 +0100

    Removed streamed packages path for MFSS 2024
    albar965/littlenavmap#1198

--- src/db/databaseloader.cpp
+++ src/db/databaseloader.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************************
-* Copyright 2015-2024 Alexander Barthel alex@littlenavmap.org
+* Copyright 2015-2025 Alexander Barthel alex@littlenavmap.org
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -229,8 +229,6 @@ void DatabaseLoader::loadScenery()
     navDatabaseOpts->setMsfsCommunityPath(FsPaths::getMsfsCommunityPath(basePath));
     navDatabaseOpts->setMsfsOfficialPath(FsPaths::getMsfsOfficialPath(basePath));
   }
-  else if(selectedFsType == atools::fs::FsPaths::MSFS_2024)
-    navDatabaseOpts->setMsfs24StreamedPackagesPath(FsPaths::getMsfs24StreamedPackagesPath());
 
   // Reset timers used in progress callback in thread context
   progressTimerElapsedThread = 0L;
