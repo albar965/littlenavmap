2d5e951ad1ff43e11533c824ebdadd6cadfbbc7f
Alexander Barthel <albar965@mailbox.org>
2025-03-23 15:00:14 +0100

    Fixed issue where map center shifted with airspaces enabled
    Placed compass rose at the right corner of the map

--- src/common/textplacement.cpp
+++ src/common/textplacement.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************************
-* Copyright 2015-2024 Alexander Barthel alex@littlenavmap.org
+* Copyright 2015-2025 Alexander Barthel alex@littlenavmap.org
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -238,7 +238,9 @@ void TextPlacement::drawTextAlongOneLine(QString text, float bearing, const QPoi
         painter->drawText(QPointF(-horizontalAdvance(txt, metrics) / 2.f, yoffset + i * metrics.height()), txt);
       }
 
-      painter->resetTransform();
+      // Bug in resetTransform() doubles the viewport size - therefore, use the inverse here
+      painter->rotate(-rotate);
+      painter->translate(-textCoord.x(), -textCoord.y());
     }
   }
 }
