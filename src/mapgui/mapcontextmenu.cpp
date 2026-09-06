16b375daf109b0779a62a114a4e6d2ce5b3e559f
Alexander Barthel <albar965@mailbox.org>
2026-09-06 12:37:21 +0200

    Added overlay sub-menu to map context menu.

--- src/mapgui/mapcontextmenu.cpp
+++ src/mapgui/mapcontextmenu.cpp
@@ -231,6 +231,16 @@ void MapContextMenu::buildMainMenu()
   else
     // No position - no sub-menu
     subMenuMore->setDisabled(true);
+
+  mapMenu.addSeparator();
+  QMenu *subMenuOverlays = new QMenu(tr("M&ap Overlays"), &mapMenu);
+  mapMenu.addMenu(subMenuOverlays);
+  // Used directly in MapWidget::contextMenuEvent()
+  subMenuOverlays->addAction(ui->actionMapOverlayOverview);
+  subMenuOverlays->addAction(ui->actionMapOverlayCompass);
+  subMenuOverlays->addAction(ui->actionMapOverlayScalebar);
+  subMenuOverlays->addAction(ui->actionMapOverlayNavigation);
+  subMenuOverlays->addAction(ui->actionMapOverlayLabelHint);
 }
 
 bool MapContextMenu::alphaSort(const map::MapBase *base1, const map::MapBase *base2)
